2cd99ab4c0f8526ecc9438df4a4dffcb414ab8a6
Michael Bloecker <michaeljbloecker@gmail.com>
2017-04-09 23:04:27 -0400

    Finished simple A Instruction

---
+++ MaxL.asm
@@ -0,0 +1,16 @@
+@0
+D=M
+@1// TEST COMMENTS
+D=D-M
+@10
+D;JGT
+@1
+D=M // we be making D=M
+@12
+0;JMP
+@0 // hey look an A instruction
+D=M
+@2
+M=D
+@14
+0;JMP // something
--- MyAssembler.java
+++ MyAssembler.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Hashtable;
-import java.util.File;
+import java.io.RandomAccessFile;
 
 public class MyAssembler {
 
@@ -14,15 +14,73 @@ public class MyAssembler {
         	int fileLine = 0;
         	int assemblyLine = 0;
         	Hashtable<String, Integer> symbolTable = new Hashtable<String, Integer>();
-        
+		RandomAccessFile file;        
+
+
         	String filename = args[0];
         	System.out.println("pensifsfdlfse");
+		try{
+			file = new RandomAccessFile(args[0], "r");
+		
+		// check for valid asm file
+		try{
+		
+		} catch(Exception f){
+		}		
+
+
+		// go through file first time
+		try{
+			file.seek(0); // go back to beginning
+		} catch(Exception f){
+		}
+
+
+                try{
+                        String line = "";
+                        String hackLine = "";
+                        while((line = file.readLine()) != null){
+
+				if(line.contains("/")){
+                                	line = line.substring(0,line.indexOf("/"));
+                                	line = line.replaceAll("\\s","");
+				}
+                                 // first take a line and determine if it is an A or C instruction
+				if(line.charAt(0) == '@'){
+                                	line = line.substring(1);
+					System.out.println(runA(line));
+				} else {
+					System.out.println(runC(line));
+				}
+                        }
+
+                } catch(Exception f){
+                }
 
 
 
+
+
+		file.close();
+		} catch(Exception e){} // end of filenotfound catch block
 
 	}
 
+	static String runA(String line){
+		String bnum = Integer.toBinaryString(Integer.parseInt(line));
+		for(int i = bnum.length(); i<15; i++){
+			bnum = "0" + bnum;
+		}
+		return "0" + bnum;
+	}
 
-
+	static String runC(String line){
+		String c = "";
+		
+		return "111" + c;
+	}
+	
+	
+	
+	
 }
